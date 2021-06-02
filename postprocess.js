a2378ae2bcd4fd694a75fe42dbad6123a89cc5ca
Raphael Rego <raphaelcarlosr@gmail.com>
2021-06-02 19:09:23 -0300

    change emoji datasource

--- postprocess.js
+++ postprocess.js
@@ -7,13 +7,14 @@ import { readJSON, writeJSON, removeFile, readTXT } from 'https://deno.land/x/fl
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const text = await readTXT(filename)
-const jsonString = text
-    .replace(/emojis:/g, '"emojis":')
-    .replace(/name:/g, '"name":')
-    .replace(/icon:/g, '"icon":')
-    .replace(/\'/g, '"')
-    .replace(/export const EMOJIS = /gi, '{"list": ') 
-    + '}';
+const jsonString = `{"list": ${text}}`
+// const jsonString = text
+//     .replace(/emojis:/g, '"emojis":')
+//     .replace(/name:/g, '"name":')
+//     .replace(/icon:/g, '"icon":')
+//     .replace(/\'/g, '"')
+//     .replace(/export const EMOJIS = /gi, '{"list": ') 
+//     + '}';
 
 const json = JSON.parse(jsonString)
 const newFilename = `emoji.json` // name of a new file to be saved
